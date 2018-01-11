682ddbe2bf86b4bfdb04816419df7ceec6dca40e
Ryan Morrison <ryanam26@yahoo.com>
2018-01-10 22:19:51 -0500

    build home page

---
+++ test/integration/pages_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesTest < ActionDispatch::IntegrationTest
+	
+	test "should get home" do 
+		get pages_home_url # pages/home URL
+		assert_response :success
+	end
+
+	test "should get root" do
+		get root_url
+		assert_response :success
+	end
+end
