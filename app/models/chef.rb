60ca586e5dfc6340c31459bc204d9f23ef1f1d38
Ryan Morrison <ryanam26@yahoo.com>
2018-01-15 22:18:07 -0500

    Start test for chef model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,5 @@
 class Chef < ApplicationRecord
+	validates :chefname, presence: true, length: { maximum: 30 }
+	validates :email, presence: true, length: { maximum: 255 }
 
 end
---
+++ test/models/chef_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+
+	def setup
+		@chef = Chef.new(chefname: "Ryan", email: "ryan@example.com")
+	end
+
+	test "should be valid" do
+		assert @chef.valid?
+	end
+
+	test "name should be present" do 
+		@chef.chefname = " "
+		assert_not @chef.valid?
+	end
+
+	test "name should be less than 30 characters" do 
+		@chef.chefname = "a" * 31
+		assert_not @chef.valid?
+	end
+
+	test "email should be present" do 
+		@chef.email = " "
+		assert_not @chef.valid?
+	end
+
+	test "email should not be too long" do 
+		@chef.email = "a" * 245 + "@example.com"
+		assert_not @chef.valid?
+	end
+
+	test "email should accept correct format" do 
+		valid_emails = %w[user@example.com RYAN@gmail.com R.first@yahoo.co john+smith@co.uk.org]
+		valid_emails.each do|valids|
+			@chef.email = valids
+			assert @chef.valid?, "#{valids.inspect} should be valid"
+		end
+	end
+end
