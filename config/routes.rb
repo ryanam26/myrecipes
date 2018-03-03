209e6289d8fdfd3e96627dd0ce6b4f108b7b5f6b
Ryan Morrison <ryanam26@yahoo.com>
2018-03-03 14:39:06 -0500

    completed recipe index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+	# root "home#index"
 	root "pages#home"
-	get "pages/home", to: "pages#home"
 
 	get '/recipes', to: 'recipes#index'
+	get '/recipes/:id', to: 'recipes#show', as: 'recipe'
 end
